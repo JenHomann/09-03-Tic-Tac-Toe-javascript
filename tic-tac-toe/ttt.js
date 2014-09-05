ca6cafc5e2a094ace2b82961f4d550dc2e5f4117
“Jen <mpjeno@gmail.com>
2014-09-04 21:38:28 -0500

    Changed some javascript code to jquery

--- tic-tac-toe/ttt.js
+++ tic-tac-toe/ttt.js
@@ -1,7 +1,7 @@
 window.onload = function(){
   
-  // Collection of all squares on the board.
-  var squares = $("td");
+  // Collection of all $squares on the board.
+  var $squares = $("td");
   var count = 0;
   
   function add_mark(){
@@ -19,13 +19,12 @@ window.onload = function(){
 	  count++;
   }
   
-  for(var i = 0; i < squares.length; i++) {
-    // When you click a square, runs the `add_mark` method.
-    squares[i].addEventListener("click", add_mark);
-	  squares[i].addEventListener("click", increment_count);
-	  squares[i].addEventListener("click", isWinningGame);
-	  squares[i].addEventListener("click", isCatsGame);
-  }
+  $squares.each(function(i){
+    $(this).on("click", add_mark);
+    $(this).on("click", increment_count);
+    $(this).on("click", isWinningGame);
+    $(this).on("click", isCatsGame);
+    });
   
   function isCatsGame(){
 	  if( count === 9 && isWinningGame() === false){
@@ -35,36 +34,36 @@ window.onload = function(){
   }
   
   function isWinningGame(){
-	  if( (squares[0].innerHTML != "-" && squares[1].innerHTML != "-" && squares[2].innerHTML != "-") && (squares[0].innerHTML === squares[1].innerHTML && squares[0].innerHTML === squares[2].innerHTML) ){
-		  alert(squares[0].innerHTML + " is the winner!");
+	  if( ($squares[0].innerHTML != "-" && $squares[1].innerHTML != "-" && $squares[2].innerHTML != "-") && ($squares[0].innerHTML === $squares[1].innerHTML && $squares[0].innerHTML === $squares[2].innerHTML) ){
+		  alert($squares[0].innerHTML + " is the winner!");
 		  location.reload();
 	  }
-	  else if( (squares[3].innerHTML != "-" && squares[4].innerHTML != "-" && squares[5].innerHTML != "-") && (squares[3].innerHTML === squares[4].innerHTML && squares[3].innerHTML === squares[5].innerHTML) ){
-		  alert(squares[3].innerHTML + " is the winner!");
+	  else if( ($squares[3].innerHTML != "-" && $squares[4].innerHTML != "-" && $squares[5].innerHTML != "-") && ($squares[3].innerHTML === $squares[4].innerHTML && $squares[3].innerHTML === $squares[5].innerHTML) ){
+		  alert($squares[3].innerHTML + " is the winner!");
 		  location.reload();
 	  }
-	  else if( (squares[6].innerHTML != "-" && squares[7].innerHTML != "-" && squares[8].innerHTML != "-") && (squares[6].innerHTML === squares[7].innerHTML && squares[6].innerHTML === squares[8].innerHTML) ){
-		  alert(squares[6].innerHTML + " is the winner!");
+	  else if( ($squares[6].innerHTML != "-" && $squares[7].innerHTML != "-" && $squares[8].innerHTML != "-") && ($squares[6].innerHTML === $squares[7].innerHTML && $squares[6].innerHTML === $squares[8].innerHTML) ){
+		  alert($squares[6].innerHTML + " is the winner!");
 		  location.reload();
 	  }
-	  else if( (squares[0].innerHTML != "-" && squares[3].innerHTML != "-" && squares[6].innerHTML != "-") && (squares[0].innerHTML === squares[3].innerHTML && squares[0].innerHTML === squares[6].innerHTML) ){
-		  alert(squares[0].innerHTML + " is the winner!");
+	  else if( ($squares[0].innerHTML != "-" && $squares[3].innerHTML != "-" && $squares[6].innerHTML != "-") && ($squares[0].innerHTML === $squares[3].innerHTML && $squares[0].innerHTML === $squares[6].innerHTML) ){
+		  alert($squares[0].innerHTML + " is the winner!");
 		  location.reload();
 	  }
-	  else if( (squares[1].innerHTML != "-" && squares[4].innerHTML != "-" && squares[7].innerHTML != "-") && (squares[1].innerHTML === squares[4].innerHTML && squares[1].innerHTML === squares[7].innerHTML) ){
-		  alert(squares[1].innerHTML + " is the winner!");
+	  else if( ($squares[1].innerHTML != "-" && $squares[4].innerHTML != "-" && $squares[7].innerHTML != "-") && ($squares[1].innerHTML === $squares[4].innerHTML && $squares[1].innerHTML === $squares[7].innerHTML) ){
+		  alert($squares[1].innerHTML + " is the winner!");
 		  location.reload();
 	  }
-	  else if( (squares[2].innerHTML != "-" && squares[5].innerHTML != "-" && squares[8].innerHTML != "-") && (squares[2].innerHTML === squares[5].innerHTML && squares[2].innerHTML === squares[8].innerHTML) ){
-		  alert(squares[2].innerHTML + " is the winner!");
+	  else if( ($squares[2].innerHTML != "-" && $squares[5].innerHTML != "-" && $squares[8].innerHTML != "-") && ($squares[2].innerHTML === $squares[5].innerHTML && $squares[2].innerHTML === $squares[8].innerHTML) ){
+		  alert($squares[2].innerHTML + " is the winner!");
 		  location.reload();
 	  }
-	  else if( (squares[0].innerHTML != "-" && squares[4].innerHTML != "-" && squares[8].innerHTML != "-") && (squares[0].innerHTML === squares[4].innerHTML && squares[0].innerHTML === squares[8].innerHTML) ){
-		  alert(squares[0].innerHTML + " is the winner!");
+	  else if( ($squares[0].innerHTML != "-" && $squares[4].innerHTML != "-" && $squares[8].innerHTML != "-") && ($squares[0].innerHTML === $squares[4].innerHTML && $squares[0].innerHTML === $squares[8].innerHTML) ){
+		  alert($squares[0].innerHTML + " is the winner!");
 		  location.reload();
 	  }
-	  else if( (squares[2].innerHTML != "-" && squares[4].innerHTML != "-" && squares[6].innerHTML != "-") && (squares[2].innerHTML === squares[4].innerHTML && squares[2].innerHTML === squares[6].innerHTML) ){
-		  alert(squares[2].innerHTML + " is the winner!");
+	  else if( ($squares[2].innerHTML != "-" && $squares[4].innerHTML != "-" && $squares[6].innerHTML != "-") && ($squares[2].innerHTML === $squares[4].innerHTML && $squares[2].innerHTML === $squares[6].innerHTML) ){
+		  alert($squares[2].innerHTML + " is the winner!");
 	  }
 	  else{
 		  return false;
